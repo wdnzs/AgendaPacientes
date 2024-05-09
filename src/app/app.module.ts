6b96d59c8688b2d8f5f2d0e05ddb6cc2dea2728d
Wilson Diniz Silva <wilson.diniz@outlook.com.br>
2024-05-09 15:17:18 -0300

    Criação dos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FinanceiroComponent } from './financeiro/financeiro.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { AgendaComponent } from './agenda/agenda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FinanceiroComponent,
+    CadastroComponent,
+    AgendaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/financeiro/financeiro.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-financeiro',
+  templateUrl: './financeiro.component.html',
+  styleUrl: './financeiro.component.scss'
+})
+export class FinanceiroComponent {
+
+}
