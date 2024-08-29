46e9a74e9646cb5e848f3626901705f71547e2de
Wilson Diniz Silva <wilson.diniz@outlook.com.br>
2024-08-29 20:29:38 -0300

    Pacientes

--- src/app/pacientes/pacientes.component.ts
+++ src/app/pacientes/pacientes.component.ts
@@ -3,15 +3,24 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+import { NgxMaskDirective, NgxMaskPipe, provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-pacientes',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatSelectModule,MatCardModule],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    ToolbarComponent,
+    NgxMaskDirective,
+    NgxMaskPipe
+  ],
   templateUrl: './pacientes.component.html',
-  styleUrl: './pacientes.component.scss',
+  styleUrls: ['./pacientes.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [provideNgxMask()]
 })
-export class PacientesComponent {
-
-}
+export class PacientesComponent { }
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,21 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarComponent {}
+
